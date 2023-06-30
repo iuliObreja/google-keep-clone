82faaa6280acc1e7cbd85e85f8f57ef43c48780d
iuli <iuliobreja23@gmail.com>
2023-06-30 14:41:34 +0300

    initializating branch

---
+++ client/Untitled-1.sh
@@ -0,0 +1,6 @@
+// how to change default terminal vscode .
+create a develop branch from main 
+switch default repository branch to develop branch
+create first commit with index.hml file on develop branch 
+read about .gitignore file 15min
+read about git flow/branch strategy 1h 
